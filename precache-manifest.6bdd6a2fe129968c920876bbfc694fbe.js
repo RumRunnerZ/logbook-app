dde11c92f7c015f21b29c58825e21d1e8dd4f538
Stef <stef.haubrich@gmail.com>
2020-09-01 15:00:08 +0200

    Updates

---
+++ precache-manifest.6bdd6a2fe129968c920876bbfc694fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8bafcc26385558c3910f59f2fd99d2",
+    "url": "/logbook-app/index.html"
+  },
+  {
+    "revision": "25ee4cee66ae8484c3f2",
+    "url": "/logbook-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a0a46e6ef2c4dec49b8",
+    "url": "/logbook-app/static/js/2.e2e9a053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/logbook-app/static/js/2.e2e9a053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ee4cee66ae8484c3f2",
+    "url": "/logbook-app/static/js/main.b6b1f054.chunk.js"
+  },
+  {
+    "revision": "01a467105d3eb298a7fb",
+    "url": "/logbook-app/static/js/runtime-main.53142228.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/logbook-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
